67ea1fd3a29919cc217147cdda9d9ed904658dc3
Michael Mikonos <mmikono@mail2agent.com>
2026-03-23 19:28:15 +0800

    zcat: show error reason for exec failure

--- apps/zcat.c
+++ apps/zcat.c
@@ -1,13 +1,20 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	char ** args = malloc(sizeof(char*) * (argc + 2)); /* For -c and terminating NULL */
+	char ** args = calloc(argc + 2, sizeof(char*));
+	if (args == NULL) {
+		perror("zcat: calloc");
+		return 1;
+	}
 	args[0] = "gunzip";
 	args[1] = "-c";
-	for (int i = 1; i < argc; ++i) {
+	for (int i = 1; i < argc; ++i)
 		args[i+1] = argv[i];
-	}
 	args[argc+1] = NULL;
-	return execvp("gunzip", args);
+	int r = execvp("gunzip", args);
+	if (r == -1)
+		perror("zcat: execvp");
+	return r;
 }
