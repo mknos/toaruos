0141698edd463e65328c374baa6a87d1e1d98932
Michael Mikonos <mmikono@mail2agent.com>
2026-04-02 16:34:27 +0800

    readlink: simplify

--- apps/readlink.c
+++ apps/readlink.c
@@ -7,26 +7,19 @@
  * Copyright (C) 2015 Mike Gerow
  */
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 #define MAX_LINK_SIZE 4096
 
-static char usage[] =
-"Usage: %s LINK\n";
-
 int main(int argc, char * argv[]) {
 	if (argc != 2) {
-		fprintf(stderr, usage, argv[0]);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "usage: %s LINK\n", argv[0]);
+		return 1;
 	}
 	char * name = argv[1];
-
 	char buf[MAX_LINK_SIZE];
-	if (readlink(name, buf, sizeof(buf)) < 0) {
-		//perror("link");
-		exit(EXIT_FAILURE);
-	}
+	if (readlink(name, buf, sizeof(buf)) == -1)
+		return 1;
 	fprintf(stdout, "%s\n", buf);
-	exit(EXIT_SUCCESS);
+	return 0;
 }
