f30f51ee640cc5c2a8e14381185135f583d17465
Michael Mikonos <mmikono@mail2agent.com>
2026-04-14 23:03:59 +0800

    libc: missing __sets_errno decl for close

--- libc/unistd/close.c
+++ libc/unistd/close.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
+#include <errno.h>
 #include <syscall.h>
 #include <syscall_nums.h>
 
 DEFN_SYSCALL1(close, SYS_CLOSE, int);
 
 int close(int file) {
-	return syscall_close(file);
+	__sets_errno(syscall_close(file));
 }
