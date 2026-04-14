c959adc3ff7fb9ed249f186a5bf114802b41eb1a
Michael Mikonos <mmikono@mail2agent.com>
2026-04-14 09:09:09 +0800

    libc: no need for early return in _handle_atexit

--- libc/stdlib/atexit.c
+++ libc/stdlib/atexit.c
@@ -4,11 +4,10 @@ static void (*_atexit_handlers[32])(void) = {NULL};
 static int _atexit_count = 0;
 
 void _handle_atexit(void) {
-	if (!_atexit_count) return;
-	do {
-		_atexit_count--;
-		_atexit_handlers[_atexit_count]();
-	} while (_atexit_count);
+	int i;
+	for (i = _atexit_count - 1; i >= 0; i--)
+		_atexit_handlers[i]();
+	_atexit_count = 0;
 }
 
 int atexit(void (*h)(void)) {
