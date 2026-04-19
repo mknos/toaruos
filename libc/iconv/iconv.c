c1f0b254a7e4857307259610c264c5d53fdaa6de
Michael Mikonos <mmikono@mail2agent.com>
2026-04-20 07:57:29 +0800

    lib: make iconv_close a stub like iconv_open

--- libc/iconv/iconv.c
+++ libc/iconv/iconv.c
@@ -10,25 +10,9 @@ struct _iconv_state {
 iconv_t iconv_open(const char *tocode, const char *fromcode) {
 	errno = EINVAL;
 	return (iconv_t)-1;
-
-#if 0
-	struct _iconv_state * state = malloc(sizeof(struct _iconv_state));
-
-	state->tocode = strdup(tocode);
-	state->fromcode = strdup(fromcode);
-
-	return (iconv_t)state;
-#endif
 }
 
 int iconv_close(iconv_t cd) {
-	struct _iconv_state * state = (struct _iconv_state*)cd;
-
-	free(state->tocode);
-	free(state->fromcode);
-
-	free(cd);
-
 	return 0;
 }
 
