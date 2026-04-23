f7310ed6883c8c16a603c36adbfe632a7cdcbf29
Michael Mikonos <mmikono@mail2agent.com>
2026-04-23 17:18:29 +0800

    cat: exit code 1 if read failed

--- apps/cat.c
+++ apps/cat.c
@@ -22,17 +22,17 @@
 static char * _argv_0;
 static char * _file;
 
-void doit(int fd) {
+int doit(int fd) {
 	char buf[CHUNK_SIZE];
 	ssize_t r;
 
-	do {
-		r = read(fd, buf, CHUNK_SIZE);
-		if (r == -1)
-			fprintf(stderr, "%s: %s: %s\n", _argv_0, _file, strerror(errno));
-		else if (r > 0)
-			write(STDOUT_FILENO, buf, r);
-	} while (r > 0);
+	while ((r = read(fd, buf, sizeof(buf))) > 0)
+		write(STDOUT_FILENO, buf, r);
+	if (r == -1) {
+		fprintf(stderr, "%s: %s: %s\n", _argv_0, _file, strerror(errno));
+		return 1;
+	}
+	return 0;
 }
 
 int main(int argc, char ** argv) {
@@ -42,7 +42,7 @@ int main(int argc, char ** argv) {
 
 	if (argc == 1) {
 		_file = "stdin";
-		doit(0);
+		return doit(0);
 	}
 
 	for (int i = 1; i < argc; ++i) {
@@ -53,13 +53,13 @@ int main(int argc, char ** argv) {
 		}
 		_file = argv[i];
 		int fd = open(argv[i], O_RDONLY);
-		if (fd < 0) {
+		if (fd == -1) {
 			fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
 			ret = 1;
 			continue;
 		}
 
-		doit(fd);
+		ret |= doit(fd);
 		close(fd);
 	}
 	return ret;
