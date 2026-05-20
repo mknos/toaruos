fbabc801b2daa52f98c604f393aa101d01bf153a
Michael Mikonos <mmikono@mail2agent.com>
2026-05-20 19:08:07 +0800

    libc: use vasprintf instead of xvasprintf in err(3)

--- libc/err/err.c
+++ libc/err/err.c
@@ -7,12 +7,14 @@
 extern char *_argv_0;
 
 void err(int rc, const char *fmt, ...) {
+	char *s;
 	va_list args;
 	va_start(args, fmt);
+	(void)vasprintf(&s, fmt, args);
+	va_end(args);
 	fputs(_argv_0, stderr);
 	fputs(": ", stderr);
-	(void)xvasprintf(cb_fprintf, stderr, fmt, args);
-	va_end(args);
+	fputs(s);
 	fputs(": ", stderr);
 	fputs(strerror(errno), stderr);
 	fputc('\n', stderr);
